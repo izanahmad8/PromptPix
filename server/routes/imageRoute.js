9eff896955507ef9fe5b758dd9ad42812c4a6387
Izan Ahmad <103888405+izanahmad8@users.noreply.github.com>
2025-03-10 17:24:06 +0530

    Delete server directory

--- server/routes/imageRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { generateImage } from "../controllers/imageController.js";
-import authMiddleware from "../middleware/auth.js";
-
-const imageRouter = express.Router();
-
-imageRouter.post("/generate-image", authMiddleware, generateImage);
-
-export default imageRouter;
